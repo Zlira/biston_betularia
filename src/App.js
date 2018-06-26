40b12338b44d6adca263a11634485c64c75baef3
zlira <olesia.hr@gmail.com>
2018-06-26 23:49:44 +0300

    Added histogram of creature distribution

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 
 import {interpolateViridis} from 'd3-scale-chromatic';
-import {scaleSequential} from 'd3-scale';
+import {scaleSequential, scaleLinear} from 'd3-scale';
+import {histogram, max, range} from 'd3-array';
 
 class App extends Component {
   render() {
@@ -12,12 +13,20 @@ class App extends Component {
   }
 }
 
+// TODO зараз чисельність популяції постійно зростає, це ОК?
+// TODO додати рух
+// TODO додати контроль клавіатурою
+// TODO додати розмноження генетикою (можливо статеве)
+// TODO додати перешкоди або окрему смугу для «полювання»
+// TODO додати історію розподілу
+
 
 class Simulation extends Component {
   constructor (props) {
     super(props)
+    // TODO set all the simulation params here
     this.createVarNum = 33
-    this.initialCreatureCount = 30
+    this.initialCreatureCount = 50
     this.maxCreatures = 300
     this.colorScale = scaleSequential(interpolateViridis)
       .domain([0, (this.createVarNum - 1)])
@@ -38,7 +47,7 @@ class Simulation extends Component {
   componentDidMount() {
     this.timerID = setInterval(
       () => this.reproduceCreatures(),
-      7000
+      2000
     );
   }
 
@@ -62,7 +71,7 @@ class Simulation extends Component {
     let maxId = Math.max(...creatures.map(cr => cr.id))
     const creaturesNum = creatures.length
     for (let i=0; i < creaturesNum; i++) {
-      if ((creaturesNum + i + 1) <= this.maxCreatures && Math.random() > .7) {
+      if ((creaturesNum + i + 1) <= this.maxCreatures && Math.random() > .95) {
         creatures.push({id: i+maxId+1, val: creatures[i].val})
       }
     }
@@ -76,11 +85,15 @@ class Simulation extends Component {
         colorScale={this.colorScale} featureVal={creature.val}
         id={creature.id} kill={this.killCreature}/>
     )
-    return (<svg width={size} height={size} className='simulation'>
-      <rect width={size} height={size} x="0" y="0"
-        fill={this.colorScale(this.createVarNum / 2)}/>
-      {creatures}
-    </svg>)
+    return (<div>
+      <svg width={size} height={size} className='simulation'>
+        <rect width={size} height={size} x="0" y="0"
+          fill={this.colorScale(this.createVarNum / 2)}/>
+        {creatures}
+      </svg>
+      <CreatureDistribution data={this.state.creatures}
+        colorScale={this.colorScale}/>
+    </div>)
   }
 }
 
@@ -99,6 +112,36 @@ class Creature extends Component {
         fill={this.props.colorScale(this.props.featureVal)}
         onClick={() => this.props.kill(this.props.id)}/>
         //stroke="white" stroke-width={.2}/>
+    )
+  }
+}
+
+class CreatureDistribution extends Component {
+  constructor(props) {
+    super(props)
+    // TODO pass bin count as param?
+    this.binCount = 33
+    this.maxCreatures = 300
+    this.histGenerator = histogram().value(d => d.val)
+      .thresholds(range(1, this.binCount))
+      .domain([0, this.binCount])
+  }
+  render() {
+    const histData = this.histGenerator(this.props.data)
+    const height = 100
+    const width = 400
+    const rectWidth = width / this.binCount
+    const yScale = scaleLinear().range([0, height])
+      .domain([0, .6])
+    console.log(histData)
+    const rects = histData.map(
+      (d, i) => <rect x={i * rectWidth} y={height - yScale(d.length / this.props.data.length)}
+                 width={rectWidth} height={yScale(d.length / this.props.data.length)} fill={this.props.colorScale(i)}/>
+    )
+    return (
+      <svg width={width} height={height}>
+        {rects}
+      </svg>
     )
   }
 }
