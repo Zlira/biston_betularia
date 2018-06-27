b6fc90130915011bc1ea006c46b5f17e01399774
zlira <olesia.hr@gmail.com>
2018-06-27 12:30:29 +0300

    Small refactoring

--- src/App.js
+++ src/App.js
@@ -19,22 +19,25 @@ class App extends Component {
 // TODO додати розмноження генетикою (можливо статеве)
 // TODO додати перешкоди або окрему смугу для «полювання»
 // TODO додати історію розподілу
+// TODO потестувати швидкодію і навантаження
 
 
 class Simulation extends Component {
   constructor (props) {
     super(props)
-    // TODO set all the simulation params here
-    this.createVarNum = 33
+    this.creatureVarsNum = 17
     this.initialCreatureCount = 50
     this.maxCreatures = 300
+    this.reproduceTick = 2000
+    this.reproduceProb = .05
+
     this.colorScale = scaleSequential(interpolateViridis)
-      .domain([0, (this.createVarNum - 1)])
+      .domain([0, (this.creatureVarsNum - 1)])
     this.state = {
       creatures: Array(this.initialCreatureCount).fill().map(
         // TODO check the extreme values probability
         (_, i) => ({
-          val: Math.round(Math.random() * (this.createVarNum - 1)),
+          val: Math.round(Math.random() * (this.creatureVarsNum - 1)),
           id: i,
         })
       )
@@ -46,8 +49,7 @@ class Simulation extends Component {
 
   componentDidMount() {
     this.timerID = setInterval(
-      () => this.reproduceCreatures(),
-      2000
+      () => this.reproduceCreatures(), this.reproduceTick
     );
   }
 
@@ -56,12 +58,11 @@ class Simulation extends Component {
   }
 
   killCreature(creatureIndex) {
-    // TODO update for asynchronous state updates
-    const creatures = this.state.creatures.filter(
-      creature => creature.id !== creatureIndex
-    )
-    this.setState({
-      'creatures': creatures,
+    this.setState((prevState, props) => {
+      const creatures = prevState.creatures.filter(
+        creature => creature.id !== creatureIndex
+      )
+      return {'creatures': creatures}
     })
   }
 
@@ -71,7 +72,7 @@ class Simulation extends Component {
     let maxId = Math.max(...creatures.map(cr => cr.id))
     const creaturesNum = creatures.length
     for (let i=0; i < creaturesNum; i++) {
-      if ((creaturesNum + i + 1) <= this.maxCreatures && Math.random() > .95) {
+      if ((creaturesNum + i + 1) <= this.maxCreatures && Math.random() < this.reproduceProb) {
         creatures.push({id: i+maxId+1, val: creatures[i].val})
       }
     }
@@ -86,13 +87,14 @@ class Simulation extends Component {
         id={creature.id} kill={this.killCreature}/>
     )
     return (<div>
-      <svg width={size} height={size} className='simulation'>
+      <svg width={size} height={size} className='simulation'
+        onDragStart={(e) => e.preventDefault()}>
         <rect width={size} height={size} x="0" y="0"
-          fill={this.colorScale(this.createVarNum / 2)}/>
+          fill={this.colorScale(this.creatureVarsNum / 2)}/>
         {creatures}
       </svg>
-      <CreatureDistribution data={this.state.creatures}
-        colorScale={this.colorScale}/>
+      <CreatureDistribution data={this.state.creatures} width={size}
+        colorScale={this.colorScale} creatureVarsNum={this.creatureVarsNum}/>
     </div>)
   }
 }
@@ -119,9 +121,7 @@ class Creature extends Component {
 class CreatureDistribution extends Component {
   constructor(props) {
     super(props)
-    // TODO pass bin count as param?
-    this.binCount = 33
-    this.maxCreatures = 300
+    this.binCount = props.creatureVarsNum
     this.histGenerator = histogram().value(d => d.val)
       .thresholds(range(1, this.binCount))
       .domain([0, this.binCount])
@@ -129,17 +129,17 @@ class CreatureDistribution extends Component {
   render() {
     const histData = this.histGenerator(this.props.data)
     const height = 100
-    const width = 400
+    const width = this.props.width
     const rectWidth = width / this.binCount
-    const yScale = scaleLinear().range([0, height])
-      .domain([0, .6])
-    console.log(histData)
+    // TODO improve scale to be consistent and nice
+    const yScale = scaleLinear().range([0, height]).domain([0, .6])
     const rects = histData.map(
       (d, i) => <rect x={i * rectWidth} y={height - yScale(d.length / this.props.data.length)}
-                 width={rectWidth} height={yScale(d.length / this.props.data.length)} fill={this.props.colorScale(i)}/>
+                 width={rectWidth} height={yScale(d.length / this.props.data.length)}
+                 fill={this.props.colorScale(i)}/>
     )
     return (
-      <svg width={width} height={height}>
+      <svg width={width} height={height} onDragStart={(e) => e.preventDefault()}>
         {rects}
       </svg>
     )
